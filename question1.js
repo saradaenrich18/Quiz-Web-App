20d1fc28a22d3bc2743767677b2398c8d2e7c217
sarada <35262650+saradaenrich18@users.noreply.github.com>
2018-06-24 20:55:25 -0700

    Update question1.js

--- question1.js
+++ question1.js
@@ -1,19 +1,19 @@
-var quoestion = [{
-		"quoestion": "The first mechanical computer designed by charles barbage was called ?",
+var question = [{
+		"question": "The first mechanical computer designed by charles barbage was called ?",
 		"option1": "Abacus",
 		"option2": "Analytical Engine",
 		"option3": "calculator",
 		"option4": "processor",
 		"answer": "2"
 		}, {
-			"quoestion": "who will become the first indian cricketer to appear in kia super leaugue in england ?",
+			"question": "who will become the first indian cricketer to appear in kia super leaugue in england ?",
 		"option1": "smt mandhana",
 		"option2": "harmanpreet kaur",
 		"option3": "mithali raj",
 		"option4": "jhulan goswami",
 		"answer": "1"
 		}, {
-			"quoestion": "who has became the first woman indian air force (IAF) fighter pilot from south indian?",
+			"question": "who has became the first woman indian air force (IAF) fighter pilot from south indian?",
 		"option1": "asistha singh",
 		"option2": "bhaanupriya",
 		"option3": "bhawana kanth",
@@ -21,35 +21,35 @@ var quoestion = [{
 		"answer": "4"
         }, {   
 	
-		"quoestion": "The International Domestic workers day (DWD) is observed on which date ?",
+		"question": "The International Domestic workers day (DWD) is observed on which date ?",
 		"option1": "june 15",
 		"option2": "june 14",
 		"option3": "june 17",
 		"option4": "june 16",
 		"answer": "4"
 		}, {
-			"quoestion": "The jawai dam is situated in which distric of rajasthan ?",
+			"question": "The jawai dam is situated in which distric of rajasthan ?",
 		"option1": "pali",
 		"option2": "chittorgarh",
 		"option3": "ajmeer",
 		"option4": "kota",
 		"answer": "1"
 	    }, {
-	    	"quoestion": "Ctrl,Shift and Alt are called............keys ?",
+	    	"question": "Ctrl,Shift and Alt are called............keys ?",
 		"option1": "modifier",
 		"option2": "function",
 		"option3": "alphanumeric",
 		"option4": "adjustment",
 		"answer": "1"
 	    }, {
-	    	"quoestion": "MS-Word is an example of ............. ?",
+	    	"question": "MS-Word is an example of ............. ?",
 		"option1": "operating system",
 		"option2": "processing device",
 		"option3": "application software",
 		"option4": "input device",
 		"answer": "3"
 	}, {
-		"quoestion": ".................. is the process of dividing the disk into tracks and sectors ?",
+		"question": ".................. is the process of dividing the disk into tracks and sectors ?",
 		"option1": "Tracking",
 		"option2": "Formatting",
 		"option3": "Crashing",
